c3a3b0ba201a9b5879eb04456a77e1e44b9514c1
OPAWALE TIMILEYIN <opawaletimi@gmail.com>
2025-01-01 23:24:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const menuIcon = document.querySelector("#menu-icon");
+const navBar = document.querySelector(".navbar");
+
+menuIcon.addEventListener('click', () => {
+navBar.classList.toggle('active');
+})
+  
